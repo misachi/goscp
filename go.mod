42c44c88891ba357154e5f39554ac2699c1f3bd9
misachi <misachibrian@gmail.com>
2020-07-12 12:54:26 +0300

    Clean up functions and add descriptions

--- go.mod
+++ go.mod
@@ -1,3 +1,14 @@
 module github.com/misachi/goscp
 
 go 1.14
+
+require (
+	github.com/cweill/gotests v1.5.3 // indirect
+	github.com/haya14busa/goplay v1.0.0 // indirect
+	github.com/mdempsky/gocode v0.0.0-20200405233807-4acdcbdea79d // indirect
+	github.com/skratchdot/open-golang v0.0.0-20200116055534-eef842397966 // indirect
+	github.com/stamblerre/gocode v1.0.0 // indirect
+	github.com/urfave/cli v1.22.4
+	github.com/urfave/cli/v2 v2.2.0
+	golang.org/x/tools v0.0.0-20200711155855-7342f9734a7d // indirect
+)
--- main.go
+++ main.go
@@ -7,28 +7,31 @@ import (
 	"os/exec"
 	"strings"
 
-	"github.com/urfave/cli"
+	"github.com/urfave/cli/v2"
 )
 
-
+// BuildArgs returns scp commandline flags as a list
 func BuildArgs(c *cli.Context) []string {
-	return []string{"-i", c.String("key"), c.String("source"), c.String("destination"),}
+	return []string{"-i", c.String("key"), c.String("source"), c.String("destination")}
 }
 
+// GetFilePath returns a slice of file path if its a combination of
+// the server name and file path e.g if path is user@1.1.1.1:/file/path, it
+// returns /file/path
 func GetFilePath(path string) string {
 	if p := strings.Index(path, ":"); p != -1 {
 		return path[p+1:]
-	} else {
-		return path
 	}
+	return path
 }
 
+// CheckFileExists ensures the file to be copied is not already in destination
 func CheckFileExists(c *cli.Context) bool {
 	path := GetFilePath(c.String("source"))
 	i := strings.LastIndex(path, "/")
-	new_path := GetFilePath(c.String("destination")) + path[i:]
+	newpath := GetFilePath(c.String("destination")) + path[i:]
 
-	_, err := os.Stat(new_path)
+	_, err := os.Stat(newpath)
 	return err == nil
 }
 
@@ -40,31 +43,31 @@ func main() {
 	app := &cli.App{
 		Flags: []cli.Flag{
 			&cli.StringFlag{
-				Name: "config",
+				Name:    "config",
 				Aliases: []string{"c"},
-				Usage: "Configurations file `FILE`",
+				Usage:   "Configurations file `FILE`",
 			},
 			&cli.StringFlag{
-				Name: "name",
+				Name:    "name",
 				Aliases: []string{"n"},
-				Usage: "Name of config to use",
+				Usage:   "Name of config to use",
 			},
 			&cli.StringFlag{
-				Name: "source",
-				Aliases: []string{"s"},
-				Usage: "Source server - Where to get the file",
+				Name:     "source",
+				Aliases:  []string{"s"},
+				Usage:    "Source server - Where to get the file",
 				Required: true,
 			},
 			&cli.StringFlag{
-				Name: "destination",
-				Aliases: []string{"d"},
-				Usage: "Destination server - Where to copy the file",
+				Name:     "destination",
+				Aliases:  []string{"d"},
+				Usage:    "Destination server - Where to copy the file",
 				Required: true,
 			},
 			&cli.StringFlag{
-				Name: "key",
-				Aliases: []string{"i"},
-				Usage: "Key to the source server",
+				Name:     "key",
+				Aliases:  []string{"i"},
+				Usage:    "Key to the source server",
 				Required: true,
 			},
 		},
@@ -75,7 +78,7 @@ func main() {
 			} else {
 				cmd := exec.Command("scp", str...)
 				if err := cmd.Run(); err != nil {
-				 	return fmt.Errorf("%v\n", err)
+					return fmt.Errorf("%v", err)
 				}
 			}
 			return nil
